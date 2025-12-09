796a7d3d9741cdb31eda3a47fff2a61716abdf0c
Aadit Chhetri <kingaadit3@gmail.com>
2025-12-09 17:05:33 +0545

    Implement Enqueue function

--- queue.c
+++ queue.c
@@ -1,23 +1,32 @@
 #include traffic.h
 
+//Queue initialization
 void initQueue(VehicleQueue *q){
-    q->front = -1;
+    q->front = 0;
     q->rear = -1;
     q->count = 0;
 }
 
+//To check whether the queue is full
 void isFull(VehicleQueue *q){
     return (q->count >= MAX_QUEUE_SIZE);
 }
 
+//To check whether the queue is empty
 void isEmpty(VehicleQueue *q){
     return (q->count == 0);
 }
 
-void enqueue(){
-
+//
+void enqueue(VehicleQueue *q,Vehicle v){
+    if(isFull()){
+        return;
+    }
+    q->(rear+1)%MAX_QUEUE_SIZE;
+    q->vehicles[q->rear] = v;
+    count++;
 }
 
 void dequeue(){
-    
+
 }
